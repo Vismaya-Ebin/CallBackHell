cb96577f2867755ac9f27d12ef12ce0d402b8de5
Vismaya Wilson <vismayawilson5@hotmail.com>
2021-12-06 23:28:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,94 @@
+// for(let i = 10; i >= 1 ; i--){
+//     document.write(i);
+//     if( i === 1){
+//         document.write("Happy Independence Day");
+//     }
+// }
+
+const numberArray = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+setTimeout(() => {
+  document.getElementById("container").innerHTML = numberArray[0];
+  setTimeout(
+    (id) => {
+      document.getElementById("container").innerHTML = id;
+      setTimeout(
+        (id) => {
+          document.getElementById("container").innerHTML = id;
+          setTimeout(
+            (id) => {
+              document.getElementById("container").innerHTML = id;
+              setTimeout(
+                (id) => {
+                  document.getElementById("container").innerHTML = id;
+                  setTimeout(
+                    (id) => {
+                      document.getElementById("container").innerHTML = id;
+                      setTimeout(
+                        (id) => {
+                          document.getElementById("container").innerHTML = id;
+                          setTimeout(
+                            (id) => {
+                              document.getElementById("container").innerHTML =
+                                id;
+                              setTimeout(
+                                (id) => {
+                                  document.getElementById(
+                                    "container"
+                                  ).innerHTML = id;
+                                  setTimeout(
+                                    (id) => {
+                                      document.getElementById(
+                                        "container"
+                                      ).innerHTML = id;
+                                      setTimeout(
+                                        () => {
+                                          document.getElementById(
+                                            "container"
+                                          ).innerHTML = "HAPPY INDEPENDENCE DAY !!!!";
+                                          document.getElementById("container").style.color = "Green";
+                                          document.getElementById("container").style.textAlign = "center";
+                                          document.getElementById("container").style.fontSize = "35px";
+                                        
+                                          
+                                        },
+                                        500,
+                                        
+                                      );
+                                    },
+                                    500,
+                                    numberArray[9]
+                                  );
+                                },
+                                500,
+                                numberArray[8]
+                              );
+                            },
+                            500,
+                            numberArray[7]
+                          );
+                        },
+                        500,
+                        numberArray[6]
+                      );
+                    },
+                    500,
+                    numberArray[5]
+                  );
+                },
+                500,
+                numberArray[4]
+              );
+            },
+            500,
+            numberArray[3]
+          );
+        },
+        500,
+        numberArray[2]
+      );
+    },
+    500,
+    numberArray[1]
+  );
+}, 500);
